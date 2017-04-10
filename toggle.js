725565db3a8f6b610feb448d6f2be93b0ea8e932
Patrick Williams <patrickw@porch.com>
2017-04-10 11:08:03 -0700

    fix toggle

---
+++ toggle.js
@@ -0,0 +1,16 @@
+var error;
+module.exports = {
+    on: function () {
+        error = console.error;
+        console.error = function (msg) {
+            error.apply(console, arguments);
+            var match = msg.match(/Warning: (Failed .* type: .*)/);
+            if (match) {
+                throw new Error(match[1]);
+            }
+        };
+    },
+    off: function () {
+        console.error = error;
+    }
+};
