94c5a21a211b0663c4f0aabb091c36c2c9a3ba35
Patrick Williams <patrickw@porch.com>
2017-04-10 10:31:45 -0700

    toggle

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-var error = console.error;
-console.error = function (msg) {
-    var ret = error.apply(console, arguments);
-    var match = msg.match(/Warning: (Failed .* type: .*)/);
-    if (match) {
-        throw new Error(match[1]);
-    }
-    return ret;
-};
+require('./toggle').on();
